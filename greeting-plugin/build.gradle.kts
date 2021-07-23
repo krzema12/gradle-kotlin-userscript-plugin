b836bf67fceb3d3090783ece6aa0846606bcc6f7
Piotr Krzeminski <git@krzeminski.it>
2021-07-23 16:28:43 +0200

    Set up publishing

--- greeting-plugin/build.gradle.kts
+++ greeting-plugin/build.gradle.kts
@@ -1,11 +1,16 @@
 plugins {
     `kotlin-dsl`
+    id("maven-publish")
+    id("com.gradle.plugin-publish") version "0.15.0"
 }
 
 repositories {
     // Use Maven Central for resolving dependencies
     mavenCentral()
 }
+
+group = "it.krzeminski"
+version = "0.1.0"
 
 dependencies {
     // Use JUnit test framework for unit tests
@@ -16,7 +21,15 @@ gradlePlugin {
     plugins {
         register("kotlin-userscript-plugin") {
             id = "kotlin-userscript"
+            displayName = "Kotlin Userscript"
+            description = "Allows creating browser userscripts in Kotlin/JS."
             implementationClass = "it.krzeminski.gradleplugins.kotlinuserscript.KotlinUserscriptPlugin"
         }
     }
+}
+
+pluginBundle {
+    website = "https://github.com/krzema12/gradle-kotlin-userscript-plugin"
+    vcsUrl = "https://github.com/krzema12/gradle-kotlin-userscript-plugin.git"
+    tags = listOf("kotlin", "userscript", "tampermonkey", "greasemonkey")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
-rootProject.name = "gradle-plugin-in-java"
+rootProject.name = "gradle-kotlin-userscript-plugin"
 
 include("greeting-plugin")

e9fda97515f646697120ca4029f4404285bd086c
Piotr Krzeminski <git@krzeminski.it>
2021-07-23 16:49:43 +0200

    Move unit test to correct directory

--- kotlin-userscript/src/test/kotlin/it/krzeminski/gradleplugins/kotlinuserscript/KotlinUserscriptPluginTest.kt
+++ kotlin-userscript/src/test/kotlin/it/krzeminski/gradleplugins/kotlinuserscript/KotlinUserscriptPluginTest.kt
@@ -1,10 +1,10 @@
-package com.example.plugin
+package it.krzeminski.gradleplugins.kotlinuserscript
 
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Assert
 import org.junit.Test
 
-class GreetingPluginTest {
+class KotlinUserscriptPluginTest {
     @Test
     fun pluginRegistersATask() {
         // Create a test project and apply the plugin
